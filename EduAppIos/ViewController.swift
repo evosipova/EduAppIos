ba0ef22b2d589a80a14fab22001d1ca26734c864
stassie-lzrv <adlazareva@edu.hse.ru>
2023-03-07 15:42:53 +0300

    Initial Commit

---
+++ EduAppIos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EduAppIos
+//
+//  Created by Настя Лазарева on 07.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
