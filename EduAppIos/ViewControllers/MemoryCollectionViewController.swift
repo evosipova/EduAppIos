39e1a65b71a71a3695faba3aaac449e9da39e71c
elizavetaosipova <evosipova_3@edu.hse.ru>
2023-05-17 16:21:24 +0300

    memory games count

--- EduAppIos/ViewControllers/MemoryCollectionViewController.swift
+++ EduAppIos/ViewControllers/MemoryCollectionViewController.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 
 class MemoryCollectionViewController: UIViewController {
@@ -186,10 +188,49 @@ class MemoryCollectionViewController: UIViewController {
             endGameContoller.resLabel.text = "win".localized
             navigationController?.pushViewController(endGameContoller, animated: true)
             restartGame()
+            updateGame3PlaysInFirestore()
         }
     }
 
+    func updateGame3PlaysInFirestore() {
+        guard let user = Auth.auth().currentUser else {
+            print("Error updating game3Plays: user not logged in")
+            return
+        }
 
+        let userRef = Firestore.firestore().collection("users").document(user.uid)
+
+        Firestore.firestore().runTransaction({ (transaction, errorPointer) -> Any? in
+            let userDocument: DocumentSnapshot
+            do {
+                try userDocument = transaction.getDocument(userRef)
+            } catch let fetchError as NSError {
+                errorPointer?.pointee = fetchError
+                return nil
+            }
+
+            guard let oldGame3Plays = userDocument.data()?["game3Plays"] as? Int else {
+                let error = NSError(
+                    domain: "AppErrorDomain",
+                    code: -1,
+                    userInfo: [
+                        NSLocalizedDescriptionKey: "Unable to retrieve game3Plays from snapshot \(userDocument)"
+                    ]
+                )
+                errorPointer?.pointee = error
+                return nil
+            }
+
+            transaction.updateData(["game3Plays": oldGame3Plays + 1], forDocument: userRef)
+            return nil
+        }) { (_, error) in
+            if let error = error {
+                print("Error updating game3Plays: \(error.localizedDescription)")
+            } else {
+                print("game3Plays successfully updated")
+            }
+        }
+    }
 
     private func restartGame() {
         selectedButtons.removeAll()
